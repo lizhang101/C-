1526f9cc136d7ec2617f74a1dc59552d3366ee74
zhali <zhali@qti.qualcomm.com>
2020-01-06 16:50:30 -0800

    ok.

---
+++ compiler_generated_functions/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+/*
+ compiler generated functions:
+ 1. copy constructor
+ 2. copy assignment operator
+ 3. destructor
+ 4. default destructor (only if no constructor declared)
+ */
+
+class ddog {
+    //the reference will stop compiler to generate the default constructor.
+    //And also this makes ddog incompatible with STL lib.
+    //fixme: string& m_name;
+
+    /* C++11 */
+    // even you defined ddog constructor, you can still use compiler's generated constructor in this way.
+    ddog() = default;
+    ddog(string name) {}
+
+};
+//equivalent to :
+class dog {
+public:
+    dog(const dog& rhs) { } // member by member initialization
+    dog& operator=(const dog& rhs) {} // member by member copying
+
+    dog() {} // 1. call base class's default constructor.
+             // 2. call data member's default constructor.
+
+    ~dog() {}   // 1. call base class destructor
+                // 2. call members' destructor.
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
